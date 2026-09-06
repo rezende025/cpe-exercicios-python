ce08f26663e556e1d40326626f4da39b3cb576d8
Matheus Rezende <mrezende025@gmail.com>
2026-09-05 21:40:17 -0300

    Create exercicio_1b_produto.py

---
+++ 01-fundamentos/exercicio_1b_produto.py
@@ -0,0 +1,9 @@
+# Coleta os dois números
+v1 = float(input("Digite o primeiro valor: "))
+v2 = float(input("Digite o segundo valor: "))
+
+# Calcula a multiplicação
+produto = v1 * v2
+
+# Exibe o resultado
+print(f"O produto é: {produto}")
