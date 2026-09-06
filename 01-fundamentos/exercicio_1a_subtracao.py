a8622d9114e3fd7f1b091e6013a48bc3323b66a0
Matheus Rezende <mrezende025@gmail.com>
2026-09-05 21:33:27 -0300

    Add subtraction functionality in exercicio_1a_subtracao.py

---
+++ 01-fundamentos/exercicio_1a_subtracao.py
@@ -0,0 +1,9 @@
+# Recebe os valores como texto e já converte para número decimal (float)
+valor1 = float(input("Digite o primeiro valor: "))
+valor2 = float(input("Digite o segundo valor: "))
+
+# Calcula a diferença
+diferenca = valor1 - valor2
+
+# Mostra o resultado na tela
+print(f"A diferença é: {diferenca}")
